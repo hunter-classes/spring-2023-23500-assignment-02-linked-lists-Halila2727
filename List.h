e1415d800c086ec5614d102883f49893c0f04f6c
Halil Akca <halil.akca45@myhunter.cuny.edu>
2023-03-01 22:45:34 -0500

    functions from video and my own

---
+++ List.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include "Node.h"
+
+class List
+{
+    private:
+    Node *head;
+    
+    public:
+    
+    List();
+    
+    void insert(std::string data);
+    void insert(int loc, std::string data);
+    int length();
+
+    std::string toString();
+    
+    ~List();
+    
+    void remove(int loc);
+    std::string get(int loc);
+    bool contains(std::string item);
+};
